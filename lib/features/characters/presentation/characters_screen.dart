45d34b5c3bdae0604c2e707c4fae6e026424a5ab
Toghrul AG <tgrlagl@proton.me>
2025-09-20 21:58:06 +0400

    bloc event + state working + showing response data from API in UI

--- lib/features/characters/presentation/characters_screen.dart
+++ lib/features/characters/presentation/characters_screen.dart
@@ -1,12 +1,42 @@
+import 'package:basic_practices/features/characters/data/character_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/characters_bloc.dart';
+import '../bloc/character_event.dart';
+import '../bloc/character_state.dart';
 
 class CharactersScreen extends StatelessWidget {
   const CharactersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Episodes Screen'),
+    return BlocProvider(
+      create: (_) =>
+          CharactersBloc(CharacterService())..add(LoadCharactersEvent()),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Characters')),
+        body: BlocBuilder<CharactersBloc, CharacterState>(
+          builder: (context, state) {
+            if (state is CharacterLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is CharacterLoaded) {
+              return ListView.builder(
+                itemCount: state.characters.length,
+                itemBuilder: (context, index) {
+                  final character = state.characters[index];
+                  return ListTile(
+                    title: Text(character.name),
+                    subtitle: Text(character.status),
+                  );
+                },
+              );
+            } else if (state is CharacterError) {
+              return Center(child: Text('Error: ${state.message}'));
+            }
+            return const Center(child: Text('Press button to load characters'));
+          },
+        ),
+      ),
     );
   }
 }
