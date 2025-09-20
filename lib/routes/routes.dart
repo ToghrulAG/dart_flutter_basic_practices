57cec4ffcd6f8e866e01b99a6a41bd804c16b448
Toghrul AG <tgrlagl@proton.me>
2025-09-20 14:32:40 +0400

    added screen routes

---
+++ lib/routes/routes.dart
@@ -0,0 +1,7 @@
+import '../features/presentation.dart';
+
+final routes = {
+  '/episodes' : (context) => const EpisodesScreen(),
+  '/characters' : (context) => const CharactersScreen(),
+  'locations' : (context) => const LocationsScreen(),
+};
