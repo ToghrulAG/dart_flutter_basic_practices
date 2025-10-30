cc8d5e35d98df735013202ea149b8cc9ce780d34
Toghrul AG <tgrlagl@proton.me>
2025-10-30 15:13:54 +0300

    modified for lazy load and pagination

--- lib/features/characters/data/character_service.dart
+++ lib/features/characters/data/character_service.dart
@@ -2,26 +2,25 @@ import 'dart:convert';
 import './character_model.dart';
 import 'package:http/http.dart' as http;
 
-
 class CharacterService {
   final String baseUrl = 'https://rickandmortyapi.com/api';
 
-  Future<List<CharacterModel>> fetchCharacters() async {
+  Future<Map<String, dynamic>> fetchCharacters({int page = 1}) async {
+    final response = await http.get(Uri.parse('$baseUrl/character?page=$page'));
 
-    final response = await http.get(Uri.parse('$baseUrl/character'));
-
-    if(response.statusCode == 200) {
-
+    if (response.statusCode == 200) {
       final data = json.decode(response.body);
 
       final List characters = data['results']; //
 
-      return characters.map((json)=> CharacterModel.fromJson(json)).toList();
-
+      return {
+        'characters': characters
+            .map((char) => CharacterModel.fromJson(char))
+            .toList(),
+        'hasNextPage': data['info']['next'] != null,
+      };
     } else {
-
       throw Exception('Failed to load characters');
-
     }
   }
 }
