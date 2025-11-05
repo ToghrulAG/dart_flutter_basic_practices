9905617af1a7821bc44e37ae5ac7038ac772f074
Toghrul AG <tgrlagl@proton.me>
2025-11-05 18:01:11 +0300

    updated for lazy loading feature

--- lib/features/characters/bloc/characters_bloc.dart
+++ lib/features/characters/bloc/characters_bloc.dart
@@ -1,22 +1,42 @@
-import 'package:flutter/widgets.dart';
+import 'package:basic_practices/features/characters/data/character_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../bloc/character_event.dart';
-import '../bloc/character_state.dart';
-import '../data/character_model.dart';
 import '../data/character_service.dart';
+import 'character_event.dart';
+import 'character_state.dart';
 
-class CharactersBloc extends Bloc<CharacterEvent, CharacterState> {
+class CharactersBloc extends Bloc<CharacterEvent, CharactersState>{
   final CharacterService service;
+  final List <CharacterModel> _allCharacters = [];
 
-  CharactersBloc(this.service) : super(CharacterInitial()) {
-    on<LoadCharactersEvent>((event, emit) async {
-      emit(CharacterLoading());
-      try {
-        final characters = await service.fetchCharacters();
-        emit(CharacterLoaded(characters));
-      } catch (e) {
-        emit(CharacterError(e.toString()));
-      }
-    });
+  int _currentPage = 1;
+  bool _isLoading = false;
+  bool _hasNextPage = true;
+
+  CharactersBloc({required this.service}) : super(CharactersInitial()) {
+    on<LoadCharactersEvent>(_onLoadCharacters);
   }
+  Future<void> _onLoadCharacters(
+    LoadCharactersEvent event, Emitter<CharactersState> emit) async {
+      if (_isLoading || !_hasNextPage) return;
+      _isLoading = true;
+
+      if(_currentPage == 1) {
+        emit(CharactersLoading());
+      }
+
+      try {
+        final response = await service.fetchCharacters(page: _currentPage);
+        final newCharacters = response['characters'] as List<CharacterModel>;
+        _hasNextPage = response['hasNextPage'] as bool;
+        _allCharacters.addAll(newCharacters);
+        _currentPage ++;
+        emit(CharactersLoaded(List.from(_allCharacters)));
+      } catch (e) {
+        emit(CharactersError(e.toString()));
+      } finally {
+        _isLoading = false;
+      }
+
+    }
+
 }
