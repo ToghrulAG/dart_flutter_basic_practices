2c2ad5e67ab594b6a4050a8ed55f7b6450f1eca1
Toghrul AG <tgrlagl@proton.me>
2025-11-10 13:45:47 +0300

    added import screen files

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,6 @@
-import '../features/presentation.dart';
+import '../features/characters/presentation/characters_screen.dart';
+import '../features/episodes/presentation/episodes_screen.dart';
+import '../features/locations/presentation/locations_screen.dart';
 
 final routes = {
   '/episodes' : (context) => const EpisodesScreen(),
